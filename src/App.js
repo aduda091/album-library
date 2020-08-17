62f6598b7f55d129468e2447cac38936b9098c12
Alen Duda <alenduda@gmail.com>
2020-08-17 18:54:33 +0200

    Initial commit after CRA cleanup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">Header</header>
+            <main>Main</main>
+        </div>
+    );
+}
+
+export default App;
