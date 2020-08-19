14fc9d325262abb54e8ff1dbcad80d8490d30d7d
Alen Duda <alen.x.duda@gsk.com>
2020-08-19 20:25:28 +0200

    started album item

---
+++ src/components/AlbumItem/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './style.scss';
+
+function AlbumItem() {
+    return (
+        <div className="album-item">
+            <div className="album-main-info">
+                <img
+                    src="https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/20/6f/b5/206fb560-6fd5-15f9-0b68-88d309ffc5a6/19UMGIM53909.rgb.jpg/170x170bb.png"
+                    alt="album cover"
+                    className="album-cover"
+                />
+                <div className="name-title-container">
+                    <div className="album-title">Feel Good Inc.</div>
+                    <div className="album-artist">Gorillaz</div>
+                </div>
+            </div>
+            
+            <div className="year-info">
+                <span className="year-label">Released: </span>
+                <span className="year-value">2005</span>
+            </div>
+        </div>
+    );
+}
+
+export default AlbumItem;
