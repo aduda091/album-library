0185d842ee2694f9de5b587fcaa2c77265b8f862
Alen Duda <alenduda@gmail.com>
2020-08-20 22:05:50 +0200

    initial api connection

--- src/components/AlbumContainer/index.jsx
+++ src/components/AlbumContainer/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './style.scss';
 import AlbumItem from '../AlbumItem';
 
-function AlbumContainer() {
-    return (
-        <div className="album-container">
-            <AlbumItem />
-            <AlbumItem />
-        </div>
-    );
+function AlbumContainer(props) {
+    const renderAlbums = () => {
+        return props.albums.map((album) => (
+            <AlbumItem
+                key={album.id}
+                id={album.id}
+                title={album.title}
+                price={album.price}
+                imageUrl={album.imageUrl}
+                releaseDate={album.releaseDate}
+                isFavorite={album.favorite}
+                artistId={album.artistId}
+                artistName={album.artist.title}
+            />
+        ));
+    };
+    return <div className="album-container">{renderAlbums()}</div>;
 }
+
+AlbumContainer.propTypes = {
+    albums: PropTypes.array
+};
 
 export default AlbumContainer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Header from '../../components/Header';
 import AlbumContainer from '../../components/AlbumContainer';
 
+import axios from '../../axios';
+import * as api from '../../constants/api';
+
+const DEFAULT_LIMIT = 10;
 
 function Home() {
+    const [albums, setAlbums] = useState([]);
+
+    // TODO: extract this to util file
+    const resolveApiUrl = () => {
+        // TODO: read limit from route param
+        const limit = DEFAULT_LIMIT;
+
+        // TODO: check if search is active to append query
+        return `${api.ALBUMS}&_limit=${limit}`;
+    };
+
+    useEffect(() => {
+        const url = resolveApiUrl();
+        axios
+            .get(url)
+            .then((response) => {
+                setAlbums(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
     return (
         <div>
             <Header title="Album list" hasSearch />
-            <AlbumContainer />
+            {albums.length ? <AlbumContainer albums={albums} /> : null}
         </div>
     );
 }
