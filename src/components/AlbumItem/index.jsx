f3f14ea52a076705fc6fcc6d606064f224e1c3ba
Alen Duda <alenduda@gmail.com>
2020-08-22 11:27:16 +0200

    enable local favorite toggling

--- src/components/AlbumItem/index.jsx
+++ src/components/AlbumItem/index.jsx
@@ -1,42 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+
+import { AlbumsContext } from '../../context/albums/albumsContext';
 
 import './style.scss';
 
 function AlbumItem(props) {
+    const toggleFavorite = useContext(AlbumsContext).toggleFavorite;
 
     const getYear = () => {
         return new Date(props.releaseDate).getFullYear();
     };
 
-    const resolveFavoriteCta = props.isFavorite ? <button className="remove-favorite">Remove favorite</button> : <button className="add-favorite">Mark as favorite</button>;
+    const resolveFavoriteCta = props.isFavorite ? (
+        <button className="remove-favorite" onClick={() => toggleFavorite(props.id)}>
+            Remove favorite
+        </button>
+    ) : (
+        <button className="add-favorite" onClick={() => toggleFavorite(props.id)}>
+            Mark as favorite
+        </button>
+    );
 
     return (
         <div className="album-item">
             <div className="album-main-info">
-                <img
-                    src={props.imageUrl}
-                    alt={`${props.title} album cover.`}
-                    className="album-cover"
-                />
+                <img src={props.imageUrl} alt={`${props.title} album cover.`} className="album-cover" />
                 <div className="name-title-container">
                     <div className="album-title">{props.title}</div>
                     <div className="album-artist">{props.artistName}</div>
                 </div>
             </div>
-            
+
             <div className="year-info">
                 <span className="year-label">Released: </span>
                 <span className="year-value">{getYear()}</span>
             </div>
 
-            <div className="price">
-                {props.price}
-            </div>
+            <div className="price">{props.price}</div>
 
-            <div className="favorite-container">
-                {resolveFavoriteCta}
-            </div>
+            <div className="favorite-container">{resolveFavoriteCta}</div>
         </div>
     );
 }
--- src/context/albums/albumsContext.js
+++ src/context/albums/albumsContext.js
@@ -2,14 +2,30 @@ import React, { createContext, useState } from 'react';
 
 export const AlbumsContext = createContext({
     albums: [],
-    searchTerm: ''
+    searchTerm: '',
+    toggleFavorite: albumId => {}
 });
 
 export default (props) => {
     const [albums, setAlbums] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+
+    const toggleFavorite = albumId => {
+        setAlbums(currentAlbums => {
+            const updatedAlbums = [...currentAlbums];
+            // TODO: API request for this
+            const albumIndex = currentAlbums.findIndex(album => album.id === albumId);
+            const newStatus = !currentAlbums[albumIndex].favorite;
+            updatedAlbums[albumIndex] = {
+                ...currentAlbums[albumIndex],
+                favorite: newStatus
+            }
+            return updatedAlbums;
+        });
+    };
+
     return (
-        <AlbumsContext.Provider value={{ albums, setAlbums, searchTerm, setSearchTerm }}>
+        <AlbumsContext.Provider value={{ albums, setAlbums, searchTerm, setSearchTerm, toggleFavorite }}>
             {props.children}
         </AlbumsContext.Provider>
     );
