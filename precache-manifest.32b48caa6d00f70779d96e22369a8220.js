ea8c04f507e9210cf7144dcbe2eb66ef16f11095
Alen Duda <alenduda@gmail.com>
2020-08-22 15:19:41 +0200

    Updates

---
+++ precache-manifest.32b48caa6d00f70779d96e22369a8220.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a0af6b78a9cec1fbb866e8df46ff15",
+    "url": "/album-library/index.html"
+  },
+  {
+    "revision": "65d43feae1faa466d812",
+    "url": "/album-library/static/css/main.5b3905b8.chunk.css"
+  },
+  {
+    "revision": "57baf4b9c0afa543df83",
+    "url": "/album-library/static/js/2.4ed015c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/album-library/static/js/2.4ed015c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d43feae1faa466d812",
+    "url": "/album-library/static/js/main.93831379.chunk.js"
+  },
+  {
+    "revision": "928a0d45fb02f1f66748",
+    "url": "/album-library/static/js/runtime-main.6231fcb4.js"
+  },
+  {
+    "revision": "2faaedc4188b5c00f608a1ddfa409b01",
+    "url": "/album-library/static/media/search.2faaedc4.svg"
+  },
+  {
+    "revision": "59ae6c95c82e7ca74130313cdb756fff",
+    "url": "/album-library/static/media/star.59ae6c95.svg"
+  }
+]);
