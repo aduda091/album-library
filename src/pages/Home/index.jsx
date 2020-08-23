05c27997e75c56946d602ebd5a56c8ba7c2f3b18
Alen Duda <alenduda@gmail.com>
2020-08-23 15:13:53 +0200

    show spinner while in loading state

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,29 +5,36 @@ import AlbumsContext from '../../context/albums/albumsContext';
 
 import Header from '../../components/Header';
 import AlbumContainer from '../../components/AlbumContainer';
+import LoadingSpinner from '../../components/LoadingSpinner';
 
 import './style.scss';
 
 function Home() {
     const urlParams = useLocation().search;
     const albumsContext = useContext(AlbumsContext);
-    const { albums, searchTerm, fetchAlbums } = albumsContext;
-    
+    const { albums, searchTerm, fetchAlbums, loading } = albumsContext;
+
     useEffect(() => {
         fetchAlbums(urlParams);
         //eslint-disable-next-line
     }, [searchTerm]);
 
+    const resolveContent = () => {
+        if (loading) return <LoadingSpinner />;
+
+        return albums.length ? (
+            <AlbumContainer albums={albums} />
+        ) : (
+            <div className="no-results">
+                No results found for <span className="search-term">{searchTerm}</span>
+            </div>
+        );
+    };
+
     return (
         <div>
             <Header title="Album list" hasSearch />
-            {albums.length ? (
-                <AlbumContainer albums={albums} />
-            ) : (
-                <div className="no-results">
-                    No results found for <span className="search-term">{searchTerm}</span>
-                </div>
-            )}
+            {resolveContent()}
         </div>
     );
 }
